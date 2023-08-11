0ba9a21fbb10c449caafa903a430b38a515c1778
Oyiad23 <heavenonearth.boakye11@gmail.com>
2023-08-11 08:52:21 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - This program prints the last digit of the number stored in the variable n.
+*main - Beginning point of program
 *
-*Return: Return 0 (correct/success)
+*Return: Return 0
 */
 int main(void)
+
 {
 	int n;
 	int last;
