be37e50cf5187722caaab1bb806df921072dc4f0
Oyiad23 <heavenonearth.boakye11@gmail.com>
2023-08-11 12:59:05 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,22 @@
 */
 int main(void)
 {
-	char alphabet;
+        char alphabet;
 
-	for (alphabet = 'a';alphabet <= 'z'; alphabet++)
-	{
-		alphabet = tolower(alphabet);
-		putchar(alphabet);
-		if (alphabet == 'z')
-		{
-			alphabet = 'A';
-			for (; alphabet <= 'Z'; alphabet++)
-			{
-				putchar(alphabet);
-			}
-			break;
-		}
-	}
-	putchar('\n');
-	return (0);
+        for (alphabet = 'a';alphabet <= 'z'; alphabet++)
+        {
+                alphabet = tolower(alphabet);
+                putchar(alphabet);
+                if (alphabet == 'z')
+                {
+                        alphabet = 'A';
+                        for (; alphabet <= 'Z'; alphabet++)
+                        {
+                                putchar(alphabet);
+                        }
+                        break;
+                }
+        }
+        putchar('\n');
+        return (0);
+}
