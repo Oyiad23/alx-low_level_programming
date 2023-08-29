248c4d947b1d600ec7cbfb9f043daaa07324c0d3
Oyiad23 <heavenonearth.boakye11@gmail.com>
2023-08-28 20:06:15 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints sum of #'s in diagnols of square
+ * @a: input square array
+ * @size: size of one dimension in array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
