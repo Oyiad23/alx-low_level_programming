9e2761e14644236329e4a3a1f06208bb33d16efc
Oyiad23 <heavenonearth.boakye11@gmail.com>
2023-09-01 15:29:11 -0700

    3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line..
+ *
+ *@str: the string to print.
+ *
+ */
+
+void _puts(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
