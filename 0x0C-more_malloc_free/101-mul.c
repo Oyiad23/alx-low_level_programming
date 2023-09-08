4eac056abcb2eaf0ca408b2848646d635141fb8e
Oyiad23 <heavenonearth.boakye11@gmail.com>
2023-09-07 18:21:40 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "main.h"
 
 /**
  * main - multiplies two positive numbers
@@ -8,28 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-        unsigned long mul;
-        int x, y;
+	unsigned long mul;
+	int x, y;
 
-        if (argc != 3)
-        {printf("Error\n");
-        exit(98);
-        }
-        for (x = 1; x < argc; x++)
-        {
-                for (y = 0; argv[x][y] != '\0'; y++)
-                {
-                        if (argv[x][y] > 57 || argv[x][y] < 48)
-                        {
-                                printf("Error\n");
-                                exit(98);
-                        }
-                }
-        }
-
-        mul = atol(argv[1]) * atol(argv[2]);
-        printf("%lu\n", mul);
-
-        return (0);
+	if (argc != 3)
+	{printf("Error\n");
+		exit(98);
+	}
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] > 57 || argv[x][y] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
 
 }
