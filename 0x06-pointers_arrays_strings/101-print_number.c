03e7a471c909bcd3fa3be736784adbbf8ab0d0ea
Oyiad23 <heavenonearth.boakye11@gmail.com>
2023-11-09 03:36:39 -0800

    modify

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,16 @@
   */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int numb = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		x = -x;
+		_putchar(45);
+		numb = -numb;
 	}
-
-	if ((x / 10)>0)
+	if (numb / 10)
 	{
-		print_number(x / 10);
+		print_number(numb / 10);
 	}
-	_putchar(x % 10 + '0');
+	_putchar(numb % 10 + '0');
 }
